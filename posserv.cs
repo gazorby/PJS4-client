ac031687d9684bfb3047634f02902b0ee263734a
skiild <dxsolar@yahoo.fr>
2019-06-25 14:59:40 +0200

    Update posserv.cs

--- posserv.cs
+++ posserv.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//script added to the clickable GameObjects to get their position as stored by the server.
 public class posserv : MonoBehaviour
 {
     public int x;
